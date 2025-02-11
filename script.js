d342a04f6288faf0cf0a3d33bdceb1270f3d21af
AndelKaz <149527003+AndelKaz@users.noreply.github.com>
2025-02-11 19:00:17 +0200

    a

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const users = [
-    { name: "Alice", eyeColor: "brown", gender: "female", isActive: true, email: "alice@gmail.com", age: 25 },
-    { name: "Bob", eyeColor: "green", gender: "male", isActive: false, email: "bob@gmail.com", age: 30 },
-    { name: "Charlie", eyeColor: "blue", gender: "male", isActive: true, email: "charlie@gmail.com", age: 28 },
-    { name: "Diana", eyeColor: "brown", gender: "female", isActive: false, email: "diana@gmail.com", age: 17 },
-    { name: "Eva", eyeColor: "green", gender: "female", isActive: true, email: "eva@gmail.com", age: 23 },
-  ];
-  const getUserNames = (users) => users.map(user => user.name);
-console.log(getUserNames(users));
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
 
-// Завдання 2: 
-// Отримати масив об'єктів користувачів за кольором очей
-const getUsersByEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
-console.log(getUsersByEyeColor(users, "blue"));
+//  Збільшуємо кількість годин гравця по id. Повернути масив гравців зі зміненою кількістю годин лише одного гравця
+ 
+ const playerId = 'player-1';
+    const increaseTime = players.map(function(player) {
+        if (player.id === playerId) {
+            return {
+            ...player,
+            timePlayed: player.timePlayed + 10
+            }
+        }
+        return player
+        })
 
-// Завдання 3: 
-// Отримати масив імен користувачів за статтю
-const getUserNamesByGender = (users, gender) => users.filter(user => user.gender === gender).map(user => user.name);
-console.log(getUserNamesByGender(users, "female"));
+//  Змінюємо властивість online на протилежну. Повернути масив гравців зі зміненою властивістю online
 
-// Завдання 4:
-// Отримати масив тільки неактивних користувачів
-const getInactiveUsers = (users) => users.filter(user => !user.isActive);
-console.log(getInactiveUsers(users));
 
-// Завдання 5: 
-// Отримати користувача за email
-const getUserByEmail = (users, email) => users.find(user => user.email === email);
-console.log(getUserByEmail(users, "bob@example.com"));
 
-// Завдання 6:
-//  Отримати масив користувачів у віковій категорії від min до max років
-const getUsersByAgeRange = (users, min, max) => users.filter(user => user.age >= min && user.age <= max);
-console.log(getUsersByAgeRange(users, 25, 35));
+const changePlayers = players.map(function (player) {
+  return {...player, online: !player.online}
+})
+
+
+
+console.log(changePlayers);
+
+
